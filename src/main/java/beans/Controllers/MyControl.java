fbb9df8089d25259af3905da8b1cc87d37b00780
Vitali <Vitali_Sharstuk@epam.com>
2019-02-24 01:43:43 +0300

    conf without mvc tag

---
+++ src/main/java/beans/Controllers/MyControl.java
@@ -0,0 +1,17 @@
+package beans.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class MyControl {
+
+    @RequestMapping(method= RequestMethod.GET)
+    public String myMeth(){
+
+        return "index";
+    }
+}
